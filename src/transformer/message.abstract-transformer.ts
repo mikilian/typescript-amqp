ad33968f222c8b7b2919ca606cd9c1c45906a4bc
michaelkilian <michael.kilian2406@gmail.com>
2020-04-25 16:31:55 +0200

    ✨ Added AbstractMessageTransformer

---
+++ src/transformer/message.abstract-transformer.ts
@@ -0,0 +1,26 @@
+/**
+ * @author      Michael Kilian
+ * @copyright   2020, https://github.com/michaelkilian
+ * @license     MIT
+ */
+
+import { IMessage }     from '../amqp';
+import { ITransformer } from './interface';
+
+export abstract class AbstractMessageTransformer<T extends IMessage> implements ITransformer<T> {
+  public transform<From>(data: From): T {
+    const response = data as unknown as T;
+
+    if (response != undefined) {
+      response.text = (encoding?: string): string => {
+        return response.content.toString(encoding || 'utf-8');
+      };
+
+      response.json = <T extends any>(): T => {
+        return JSON.parse(response.text());
+      };
+    }
+
+    return response;
+  }
+}
