4d46750f7305957887b03981dab6205a59faf2bf
michaelkilian <michael.kilian2406@gmail.com>
2020-04-25 17:03:55 +0200

    ⚡️ Added MissingConnectionException

---
+++ src/exception/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @author      Michael Kilian
+ * @copyright   2020, https://github.com/michaelkilian
+ * @license     MIT
+ */
+
+export * from './connection-failed.exception';
+export * from './invalid-connection-url.exception';
+export * from './missing-connection.exception';
