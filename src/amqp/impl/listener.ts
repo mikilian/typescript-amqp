e8697c8c362267494eebed52dadf4f6de2eaa287
michaelkilian <michael.kilian2406@gmail.com>
2020-04-25 18:35:40 +0200

    ✨ Added basic listener and sender

---
+++ src/amqp/impl/listener.ts
@@ -0,0 +1,43 @@
+/**
+ * @author      Michael Kilian
+ * @copyright   2020, https://github.com/michaelkilian
+ * @license     MIT
+ */
+
+import { Connection }                       from 'amqplib';
+import { AbstractAmqpConnection, IMessage } from '../interfaces';
+import {
+  IMessageParameterTransformer,
+  IMessageTransformer
+} from '../../transformer';
+
+export class AmqpListener extends AbstractAmqpConnection {
+  public constructor(
+    private readonly queue:      string,
+    connection:                  Connection,
+    messageParameterTransformer: IMessageParameterTransformer,
+    messageTransformer:          IMessageTransformer
+  )
+  {
+    super(connection, messageParameterTransformer, messageTransformer);
+  }
+
+  public async create(callback: (msg: IMessage | null) => any) {
+    const channel = await this.connection.createChannel();
+
+    await channel.assertQueue(this.queue, { durable: false });
+    await channel.consume(this.queue, msg => callback(this.messageTransformer.transform(msg)), {
+      noAck: true
+    });
+  }
+
+  public async send(data: string): Promise<void>
+  public async send(data: object): Promise<void>
+  public async send(data: Buffer): Promise<void>
+  public async send(data: string | object | Buffer): Promise<void> {
+    const channel = await this.connection.createChannel();
+
+    await channel.assertQueue(this.queue, { durable: false });
+    await channel.sendToQueue(this.queue, this.messageParameterTransformer.transform(data));
+  }
+}
